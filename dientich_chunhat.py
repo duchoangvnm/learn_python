c881b7ef65c58491353f4429e52dd337053f6671
duchoangvnm <duchoangvnm@gmail.com>
2020-09-05 18:46:19 +0700

    Add files via upload

---
+++ dientich_chunhat.py
@@ -0,0 +1,14 @@
+#Tính diện tích hình chữ nhật 
+def compule_rectangle_area(height,width):
+	area = height*width
+	return area
+#test case 
+#(h-5,w=6) -> area = 30
+#(10,w=4) -> area = 40
+
+areal = compule_rectangle_area(5,6)
+print(areal)
+
+
+areal = compule_rectangle_area(10,4)
+print(areal)
